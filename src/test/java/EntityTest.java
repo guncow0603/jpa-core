d86c2907e0e88b8a4d619d65e192feb5eb010d18
guncow0603 <guncow0603@gmail.com>
2023-11-06 16:31:51 +0900

    Entity 이해하기

--- src/test/java/EntityTest.java
+++ src/test/java/EntityTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class EntityTest {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EntityTest {
+
+    EntityManagerFactory emf;
+    EntityManager em;
+
+    @BeforeEach
+    void setUp() {
+        emf = Persistence.createEntityManagerFactory("memo");
+        em = emf.createEntityManager();
+    }
+
+    @Test
+    void test1() {
+
+    }
 }
